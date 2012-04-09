4f2db3163fcffa5845888d4284223aecda482749
Carlos Guerreiro <carlos@perceptiveconstructs.com>
2012-04-09 23:59:39 +0300

    run the test under jsdom

---
+++ test/test.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var jsdom = require('jsdom');
+
+jsdom.env({
+  html: 'test-data-graft.html',
+  scripts: [
+    '../stuff/jquery-1.7.1.min.js',
+    '../data-graft.js',
+    'test-data-graft.js'
+  ],
+  done: function(errors, window) {
+    if(errors)
+      throw errors;
+    window.console = console;
+    window.test(function(errors) {
+      if(errors.length === 0) {
+        console.log('OK');
+        return;
+      }
+      console.error(errors.length, 'errors');
+      errors.forEach(function(error) {
+        console.log('in '+ error[0]+ '.'+ error[1]);
+        // FIXME: details of mismatch - perhaps compareDOM could return them?
+      });
+    });
+  }});
